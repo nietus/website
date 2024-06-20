66e22d8ee7d507854c8876e2473e593ff814eeac
niet <antonionetodev@yahoo.com>
2024-06-20 17:41:01 -0300

    eslint rule fix

--- app/page.js
+++ app/page.js
@@ -1,22 +1,19 @@
 // app/page.js
 
-import Layout from './layout';
 import Link from 'next/link';
 
-export default function Page() {
+export default function HomePage() {
   return (
-    <Layout>
-      <div className="flex justify-center items-center min-h-screen bg-gray-100">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
-          <p className="mb-4">This is the landing page of Antonio Neto's website.</p>
-          <Link href="/curriculum">
-            <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-              Go to Curriculum
-            </button>
-          </Link>
-        </div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
+        <p className="mb-4">This is the landing page of Antonio Neto&apos;s website.</p>
+        <Link href="/curriculum">
+          <a className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
+            Go to Curriculum
+          </a>
+        </Link>
       </div>
-    </Layout>
+    </div>
   );
 }
