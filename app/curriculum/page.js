8db39b2eda103363e57a5cb78f7920b2f3e40ac9
niet <antonionetodev@yahoo.com>
2024-06-21 20:42:58 -0300

    add: working version v1

--- app/curriculum/page.js
+++ app/curriculum/page.js
@@ -1,33 +1,35 @@
-// app/curriculum/page.js
+// app/page.js
 
 "use client";
 
-import React, { useState } from 'react';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
-import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import Image from "next/image";
+import { FaDownload } from "react-icons/fa";
+import { FlagIcon } from 'react-flag-kit';
 
 const Curriculum = () => {
-  const [language, setLanguage] = useState('en');
+  const [language, setLanguage] = useState("en");
+  const [isLoading, setIsLoading] = useState(false);
 
   const generatePDF = () => {
-    const input = document.getElementById('curriculum');
+    const input = document.getElementById("curriculum");
+    setIsLoading(true);
     html2canvas(input, { scale: 2 }).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
+      const imgData = canvas.toDataURL("image/png");
       const pdf = new jsPDF({
-        orientation: 'portrait',
-        unit: 'pt',
-        format: 'a4',
+        orientation: "portrait",
+        unit: "pt",
+        format: "a4",
       });
       const imgProps = pdf.getImageProperties(imgData);
       const pdfWidth = pdf.internal.pageSize.getWidth();
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-      pdf.save('Curriculum_Antonio_Neto.pdf');
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+      pdf.save("Curriculum_Antonio_Neto.pdf");
+      setIsLoading(false);
     });
   };
 
@@ -36,52 +38,94 @@ const Curriculum = () => {
     return (
       <div className="flex">
         {Array.from({ length: totalStars }, (v, i) => (
-          <span key={i} className={i < rating ? 'text-yellow-500' : 'text-gray-300'}>★</span>
+          <span
+            key={i}
+            className={i < rating ? "text-yellow-500" : "text-gray-300"}
+          >
+            ★
+          </span>
         ))}
       </div>
     );
   };
 
   const switchLanguage = () => {
-    setLanguage((prevLang) => (prevLang === 'en' ? 'pt' : 'en'));
+    setLanguage((prevLang) => (prevLang === "en" ? "pt" : "en"));
   };
 
   const translations = {
     en: {
-      title: 'Antonio Neto',
-      subtitle: 'Student at PUC Minas',
-      education: 'Education',
-      experience: 'Experience',
-      skills: 'Skills',
-      articles: 'Articles/Blog',
-      bachelorDegree: 'Bachelor of Computer Science, PUC Minas',
-      javaDeveloper: 'Java Developer, XYZ Company',
-      downloadPDF: 'Download PDF',
+      title: "Antonio Neto",
+      subtitle: "Student at PUC Minas",
+      education: "Education",
+      jobexperience: "Experience",
+      skills: "Skills",
+      projects: "Projects",
+      degree: "Bachelor of Computer Science (2026/2)",
+      experience: "Open-Source and College Projects",
+      projectList: [
+        {
+          title: "Estoque Master",
+          description: "A project to manage stock efficiently.",
+          link: "https://github.com/nietus/spark_crud",
+        },
+        {
+          title: "AnimeDB",
+          description: "Use of cryptography algorithms, pattern matching, data structures, and compression in an anime database.",
+          link: "https://github.com/AnimeAEDS3/aeds3",
+        },
+      ],
     },
     pt: {
-      title: 'Antonio Neto',
-      subtitle: 'Estudante na PUC Minas',
-      education: 'Educação',
-      experience: 'Experiência',
-      skills: 'Habilidades',
-      articles: 'Artigos/Blog',
-      bachelorDegree: 'Bacharel em Ciência da Computação, PUC Minas',
-      javaDeveloper: 'Desenvolvedor Java, Empresa XYZ',
-      downloadPDF: 'Baixar PDF',
-    }
+      title: "Antonio Neto",
+      subtitle: "Estudante na PUC Minas",
+      education: "Educação",
+      jobexperience: "Experiência",
+      skills: "Habilidades",
+      projects: "Projetos",
+      degree: "Bacharel em Ciência da Computação (2026/2)",
+      experience: "Open-Source e projetos de faculdade",
+      projectList: [
+        {
+          title: "Estoque Master",
+          description: "Um projeto para gerenciar estoque de forma eficiente.",
+          link: "https://github.com/nietus/spark_crud",
+        },
+        {
+          title: "AnimeDB",
+          description: "Uso de algoritmos de criptografia, casamento de padrões, estruturas de dados e compactação em um banco de dados de anime.",
+          link: "https://github.com/AnimeAEDS3/aeds3",
+        },
+      ],
+    },
   };
 
   const t = translations[language];
 
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-gradient-to-r from-white to-gray-100 dark:bg-gradient-to-r dark:from-gray-900 dark:to-gray-800 font-sans">
-      <div className="absolute top-4 right-4">
-        <label className="switch">
-          <input type="checkbox" onChange={switchLanguage} />
-          <span className="slider round"></span>
-        </label>
+      {isLoading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="text-white text-2xl">Generating PDF...</div>
+        </div>
+      )}
+      <div className="absolute top-4 right-4 flex items-center space-x-4 no-print">
+        <div className="flex items-center space-x-2">
+          <FlagIcon code="US" size={32} className={`cursor-pointer ${language === 'en' ? 'opacity-100' : 'opacity-50'}`} onClick={() => setLanguage('en')} />
+          <FlagIcon code="BR" size={32} className={`cursor-pointer ${language === 'pt' ? 'opacity-100' : 'opacity-50'}`} onClick={() => setLanguage('pt')} />
+        </div>
+        <button
+          onClick={generatePDF}
+          className="bg-green-600 dark:bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-700 dark:hover:bg-green-600 transition-transform transform hover:scale-105 duration-300 flex items-center"
+        >
+          <FaDownload className="mr-2" />
+          PDF
+        </button>
       </div>
-      <div id="curriculum" className="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-lg max-w-3xl w-full h-5/6 overflow-y-auto scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-blue-200 dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-800 rounded-scrollbar">
+      <div
+        id="curriculum"
+        className="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-2xl max-w-3xl w-11/12 h-5/6 md:w-4/6 md:h-4/5 overflow-hidden"
+      >
         <div className="flex items-center mb-6">
           <div className="w-20 h-20 relative rounded-full overflow-hidden shadow-lg">
             <Image
@@ -92,56 +136,62 @@ const Curriculum = () => {
             />
           </div>
           <div className="ml-4">
-            <h1 className="text-4xl font-bold text-primary dark:text-white mb-1">{t.title}</h1>
-            <p className="text-lg text-primary dark:text-gray-400">{t.subtitle}</p>
+            <h1 className="text-4xl font-bold text-primary dark:text-white mb-1">
+              {t.title}
+            </h1>
+            <p className="text-lg text-primary dark:text-gray-400">
+              {t.subtitle}
+            </p>
           </div>
         </div>
-        <div className="mb-6">
-          <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-blue-500 pb-2">{t.education}</h2>
-          <p className="text-lg mb-4 dark:text-gray-300">{t.bachelorDegree}</p>
-          <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-blue-500 pb-2">{t.experience}</h2>
-          <p className="text-lg mb-4 dark:text-gray-300">{t.javaDeveloper}</p>
-          <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-blue-500 pb-2">{t.skills}</h2>
-          <ul className="text-lg mb-4 dark:text-gray-300 space-y-2">
-            <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
-              <span>Java</span> {renderStars(5)}
-            </li>
-            <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
-              <span>Python</span> {renderStars(4)}
-            </li>
-            <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
-              <span>HTML</span> {renderStars(4)}
-            </li>
-            <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
-              <span>CSS</span> {renderStars(3)}
-            </li>
-            <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
-              <span>JavaScript</span> {renderStars(4)}
-            </li>
-          </ul>
+        <div id="curriculum-content">
+          <div className="mb-6">
+            <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+              {t.education}
+            </h2>
+            <p className="text-lg mb-4 dark:text-gray-300">{t.degree}</p>
+            <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+              {t.jobexperience}
+            </h2>
+            <p className="text-lg mb-4 dark:text-gray-300">{t.experience}</p>
+            <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+              {t.skills}
+            </h2>
+            <ul className="text-lg mb-4 dark:text-gray-300 space-y-2">
+              <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
+                <span>Java</span> {renderStars(4)}
+              </li>
+              <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
+                <span>Python</span> {renderStars(3)}
+              </li>
+              <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
+                <span>Docker</span> {renderStars(2)}
+              </li>
+              <li className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300">
+                <span>Machine Learning</span> {renderStars(2)}
+              </li>
+            </ul>
+          </div>
+          <div className="mb-6">
+            <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+              {t.projects}
+            </h2>
+            <ul className="text-lg mb-4 dark:text-gray-300 list-disc list-inside space-y-2">
+              {t.projectList.map((project, index) => (
+                <li key={index}>
+                  <a
+                    target="_blank"
+                    href={project.link}
+                    className="text-green-500 dark:text-green-300 transition-colors duration-300 hover:text-green-700 dark:hover:text-green-500"
+                  >
+                    {project.title}
+                  </a>
+                  <p className="text-sm dark:text-gray-400">{project.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="mb-8">
-          <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-blue-500 pb-2">{t.articles}</h2>
-          <ul className="text-lg mb-4 dark:text-gray-300 list-disc list-inside space-y-2">
-            <li><a href="#" className="text-blue-500 dark:text-blue-300 transition-colors duration-300 hover:text-blue-700 dark:hover:text-blue-500">My First Article</a></li>
-            <li><a href="#" className="text-blue-500 dark:text-blue-300 transition-colors duration-300 hover:text-blue-700 dark:hover:text-blue-500">Exploring JavaScript</a></li>
-            <li><a href="#" className="text-blue-500 dark:text-blue-300 transition-colors duration-300 hover:text-blue-700 dark:hover:text-blue-500">Understanding CSS</a></li>
-          </ul>
-        </div>
-        <div className="flex justify-center space-x-4 mb-8">
-          <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="text-blue-600 dark:text-blue-400 transition-transform transform hover:scale-110 duration-300">
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-          </a>
-          <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-blue-600 dark:text-blue-400 transition-transform transform hover:scale-110 duration-300">
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-          </a>
-          <a href="mailto:your-email@example.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 dark:text-blue-400 transition-transform transform hover:scale-110 duration-300">
-            <FontAwesomeIcon icon={faEnvelope} size="2x" />
-          </a>
-        </div>
-        <button onClick={generatePDF} className="mt-4 w-full bg-blue-600 dark:bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-700 dark:hover:bg-blue-600 transition-transform transform hover:scale-105 duration-300">
-          {t.downloadPDF}
-        </button>
       </div>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -1,16 +1,150 @@
 // app/page.js
 
+"use client";
+
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import { FlagIcon } from 'react-flag-kit';
 
 export default function HomePage() {
+  const [language, setLanguage] = useState('en');
+  const [key, setKey] = useState(0);
+
+  const translations = {
+    en: {
+      welcome: 'Hello, World!',
+      goToCurriculum: 'Explore My Curriculum',
+      iAm: 'I am a',
+      aboutMe: 'Hi, I am Antonio Neto, a passionate learner and developer. I love working on exciting projects and continuously improving my skills. I like to work with Intelligent Systems and Databases.',
+      education: 'Education',
+      degree: 'Bachelor of Computer Science (2026/2)',
+      skills: 'Skills',
+      skillsList: [
+        'Java', 'Python', 'Docker', 'Machine Learning'
+      ],
+      projects: 'Projects',
+      words: ['Learner', 'Developer', 'Scientist'],
+      projectList: [
+        {
+          title: "Estoque Master",
+          description: "A project to manage stock efficiently.",
+          link: "https://github.com/nietus/spark_crud",
+        },
+        {
+          title: "AnimeDB",
+          description: "Use of cryptography algorithms, pattern matching, data structures, and compression in an anime database.",
+          link: "https://github.com/AnimeAEDS3/aeds3",
+        },
+      ],
+      experience: 'Experience',
+      experienceDetails: 'Open-Source and College Projects',
+    },
+    pt: {
+      welcome: 'Olá, Mundo!',
+      goToCurriculum: 'Explore Meu Currículo',
+      iAm: 'Eu sou um',
+      aboutMe: 'Oi, eu sou Antonio Neto, um estudante e desenvolvedor apaixonado. Adoro trabalhar em projetos empolgantes e melhorar continuamente minhas habilidades. Gosto de trabalhar com Sistemas Inteligentes e Bancos de Dados.',
+      education: 'Educação',
+      degree: 'Bacharel em Ciência da Computação (2026/2)',
+      skills: 'Habilidades',
+      skillsList: [
+        'Java', 'Python', 'Docker', 'Machine Learning'
+      ],
+      projects: 'Projetos',
+      words: ['Estudante', 'Desenvolvedor', 'Cientista'],
+      projectList: [
+        {
+          title: "Estoque Master",
+          description: "Um projeto para gerenciar estoque de forma eficiente.",
+          link: "https://github.com/nietus/spark_crud",
+        },
+        {
+          title: "AnimeDB",
+          description: "Uso de algoritmos de criptografia, casamento de padrões, estruturas de dados e compactação em um banco de dados de anime.",
+          link: "https://github.com/AnimeAEDS3/aeds3",
+        },
+      ],
+      experience: 'Experiência',
+      experienceDetails: 'Open-Source e projetos de faculdade',
+    },
+  };
+
+  const t = translations[language];
+  const [text] = useTypewriter({
+    words: t.words,
+    loop: {},
+    key,
+  });
+
+  useEffect(() => {
+    setKey((prevKey) => prevKey + 1);
+  }, [language]);
+
+  const switchLanguage = () => {
+    setLanguage((prevLang) => (prevLang === 'en' ? 'pt' : 'en'));
+    setKey((prevKey) => prevKey + 1);
+  };
+
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
-        <Link href="/curriculum" className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-            Go to Curriculum
-        </Link>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Antonio Neto's Website</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" />
+      </Head>
+      <div className="relative flex min-h-[100vh] bg-gray-100 animate-fade-in">
+        <div className="hidden md:block w-1/4 bg-green-200 p-8 flex flex-col items-center">
+          <h2 className="text-2xl font-bold mb-4">{language === 'en' ? 'About Me' : 'Sobre Mim'}</h2>
+          <p className="mb-4">{t.aboutMe}</p>
+          <div className="mb-6">
+            <h3 className="text-xl font-semibold mb-2">{t.education}</h3>
+            <p>{t.degree}</p>
+          </div>
+          <div className="mb-6">
+            <h3 className="text-xl font-semibold mb-2">{t.skills}</h3>
+            <ul>
+              {t.skillsList.map((skill, index) => (
+                <li key={index}>{skill}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="mb-6">
+            <h3 className="text-xl font-semibold mb-2">{t.experience}</h3>
+            <p>{t.experienceDetails}</p>
+          </div>
+          <div className="flex space-x-4 mt-4">
+            <a href="https://www.linkedin.com/in/antonioniet/" target="_blank" rel="noopener noreferrer" className="text-green-600 hover:text-green-800 transition-transform transform hover:scale-110">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+            <a href="https://github.com/nietus" target="_blank" rel="noopener noreferrer" className="text-green-600 hover:text-green-800 transition-transform transform hover:scale-110">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="mailto:antonio.couto@sga.pucminas.br" target="_blank" rel="noopener noreferrer" className="text-green-600 hover:text-green-800 transition-transform transform hover:scale-110">
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </a>
+          </div>
+        </div>
+        <div className="flex-1 flex flex-col justify-center items-center p-8 md:p-0">
+          <div className="absolute top-4 right-4 flex items-center space-x-4">
+            <FlagIcon code="US" size={32} className={`cursor-pointer ${language === 'en' ? 'opacity-100' : 'opacity-50'}`} onClick={() => switchLanguage()} />
+            <FlagIcon code="BR" size={32} className={`cursor-pointer ${language === 'pt' ? 'opacity-100' : 'opacity-50'}`} onClick={() => switchLanguage()} />
+          </div>
+          <div className="text-center">
+            <h1 className="text-5xl font-light mb-4 text-green-700" style={{ fontFamily: 'Roboto, sans-serif' }}>{t.welcome}</h1>
+            <div className="text-2xl mb-4" style={{ fontFamily: 'Roboto, sans-serif' }}>
+              {t.iAm} <span className="text-green-500">{text}<Cursor /></span>
+            </div>
+            <Link href="/curriculum" className="mt-4 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700 inline-block transition-transform transform hover:scale-105">
+              {t.goToCurriculum}
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
