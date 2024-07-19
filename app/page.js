669712b1bf41fdf968fba9fe862ddfaaf8d0dbcf
antonionetodev@yahoo.com <antonionetodev@yahoo.com>
2024-07-19 18:21:55 -0300

    atualizacao pdfs

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
 import Image from "next/image";
 import { FaDownload, FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 import { FlagIcon } from "react-flag-kit";
@@ -10,13 +8,16 @@ import translations from "./translations.json";
 
 const MainPage = () => {
   const [language, setLanguage] = useState("pt");
-  const [isLoading, setIsLoading] = useState(false);
   const [loaded, setLoaded] = useState(false);
+  const [initialLoad, setInitialLoad] = useState(true);
+  const [backgroundStyle, setBackgroundStyle] = useState({});
 
   const sectionsRef = useRef([]);
 
   useEffect(() => {
     setLoaded(true);
+    setInitialLoad(false); // Mark initial load as complete after first render
+    changeBackground("pt"); // Set initial background for pt
   }, []);
 
   useEffect(() => {
@@ -44,51 +45,6 @@ const MainPage = () => {
     };
   }, [sectionsRef]);
 
-  const generatePDF = () => {
-    const input = document.getElementById("curriculum");
-    setIsLoading(true);
-
-    // Temporarily expand the element height to capture all content
-    const originalHeight = input.style.height;
-    input.style.height = "auto";
-    input.style.maxHeight = "none";
-
-    // Scroll to top before capturing
-    window.scrollTo(0, 0);
-
-    html2canvas(input, { scale: 2, scrollY: -window.scrollY }).then(
-      (canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-        const pdf = new jsPDF({
-          orientation: "portrait",
-          unit: "pt",
-          format: "a4",
-        });
-        const imgProps = pdf.getImageProperties(imgData);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-
-        pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-        pdf.setFontSize(10);
-        pdf.setTextColor(0, 0, 255);
-        pdf.textWithLink(
-          "https://antoniocouto.vercel.app",
-          20,
-          pdf.internal.pageSize.getHeight() - 30,
-          { url: "https://antoniocouto.vercel.app" }
-        );
-
-        pdf.save("Curriculum_Antonio_Neto.pdf");
-
-        // Restore the original height
-        input.style.height = originalHeight;
-        input.style.maxHeight = "";
-
-        setIsLoading(false);
-      }
-    );
-  };
-
   const renderStars = (rating) => {
     const totalStars = 5;
     return (
@@ -108,22 +64,50 @@ const MainPage = () => {
   };
 
   const switchLanguage = () => {
-    setLanguage((prevLang) => (prevLang === "pt" ? "en" : "pt"));
+    setLanguage((prevLang) => {
+      const newLang = prevLang === "pt" ? "en" : "pt";
+      changeBackground(newLang);
+      return newLang;
+    });
+  };
+
+  const changeBackground = (lang) => {
+    if (lang === "en") {
+      setBackgroundStyle({
+        backgroundImage: `
+          radial-gradient(circle at 50% 50%, rgba(66, 135, 245, 0.3) 0%, rgba(255, 135, 245, 0.1) 100%),
+          linear-gradient(to bottom, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)),
+          linear-gradient(90deg, rgba(66, 135, 245, 0.2), rgba(66, 135, 245, 0.2)),
+          linear-gradient(45deg, rgba(66, 135, 245, 0.2), rgba(66, 135, 245, 0.2))`,
+        backgroundSize: `150px 150px, 100% 60px, 100% 1px, 100% 1px`,
+        backgroundPosition: `0 0, 0 0, 0 50%, 0 30%`,
+        backgroundRepeat: `repeat, repeat, repeat, repeat`,
+        backgroundAttachment: `fixed`,
+      });
+    } else {
+      setBackgroundStyle({
+        backgroundImage: `
+          radial-gradient(circle at 50% 50%, rgba(66, 153, 225, 0.4) 0%, rgba(66, 153, 225, 0.1) 100%),
+          linear-gradient(to bottom, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)),
+          linear-gradient(90deg, rgba(0, 255, 0, 0.2), rgba(0, 255, 0, 0.2)),
+          linear-gradient(45deg, rgba(0, 255, 0, 0.2), rgba(0, 255, 0, 0.2))`,
+        backgroundSize: `150px 150px, 100% 60px, 100% 1px, 100% 1px`,
+        backgroundPosition: `0 0, 0 0, 0 50%, 0 30%`,
+        backgroundRepeat: `repeat, repeat, repeat, repeat`,
+        backgroundAttachment: `fixed`,
+      });
+    }
   };
 
   const t = translations[language];
 
   return (
     <div
-      className={`min-h-screen flex flex-col justify-between bg-gradient-to-r from-gray-100 to-green-100 dark:bg-gradient-to-r dark:from-gray-800 dark:to-gray-900 font-sans ${
-        loaded ? "opacity-100" : "opacity-0"
+      className={`min-h-screen flex flex-col justify-between font-sans ${
+        initialLoad ? "opacity-0" : "opacity-100"
       } transition-opacity duration-1000`}
+      style={backgroundStyle}
     >
-      {isLoading && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="text-white text-2xl">Generating PDF...</div>
-        </div>
-      )}
       <div className="flex-grow flex flex-col justify-center items-center mt-[10vh]">
         <div className="absolute top-4 right-4 flex items-center space-x-4 no-print">
           <div className="flex items-center space-x-2">
@@ -133,7 +117,7 @@ const MainPage = () => {
               className={`cursor-pointer ${
                 language === "en" ? "opacity-100" : "opacity-50"
               } hover:opacity-100 transition-opacity duration-300`}
-              onClick={() => setLanguage("en")}
+              onClick={() => switchLanguage("en")}
             />
             <FlagIcon
               code="BR"
@@ -141,22 +125,32 @@ const MainPage = () => {
               className={`cursor-pointer ${
                 language === "pt" ? "opacity-100" : "opacity-50"
               } hover:opacity-100 transition-opacity duration-300`}
-              onClick={() => setLanguage("pt")}
+              onClick={() => switchLanguage("pt")}
             />
           </div>
-          <button
-            onClick={generatePDF}
-            className="bg-green-600 dark:bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-700 dark:hover:bg-green-600 transition-transform transform hover:scale-105 duration-300 flex items-center hidden md:flex"
-          >
+          <a
+            href={
+              language === "en"
+                ? "/Antonio_Neto_Curriculum.pdf"
+                : "/Antonio_Neto_Curriculum (1).pdf"
+            }
+            className={`py-2 px-4 rounded-full transition-transform transform hover:scale-105 duration-300 items-center flex ${
+              language === "en"
+                ? "bg-blue-500 dark:bg-blue-500 text-white hover:bg-blue-600 dark:hover:bg-blue-600"
+                : "bg-green-600 dark:bg-green-500 text-white hover:bg-green-700 dark:hover:bg-green-600"
+            }`}
+            download
+            >
             <FaDownload className="mr-2" />
-            PDF
-          </button>
+            CV
+          </a>
         </div>
         <div
           id="curriculum"
           className="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-2xl max-w-2xl w-11/12 md:w-4/6"
+          style={{ color: "black" }} // Ensure text color is solid black
         >
-          <div className="flex items-center mb-6 animate-fade-in-down">
+          <div className="flex items-center mb-6">
             <div className="w-20 h-20 relative rounded-full overflow-hidden shadow-lg">
               <Image
                 src="/profile_picture.jpeg"
@@ -175,78 +169,77 @@ const MainPage = () => {
             </div>
           </div>
           <div id="curriculum-content">
-            <div
-              className="mb-6 animate-fade-in-down"
-              ref={(el) => (sectionsRef.current[0] = el)}
-            >
-              <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+            <div className="mb-6" ref={(el) => (sectionsRef.current[0] = el)}>
+              <h2
+                className={`text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 ${
+                  language === "en" ? "border-blue-400" : "border-green-500"
+                } pb-2`}
+              >
                 {t.aboutMeTitle}
               </h2>
               <p className="text-lg mb-4 dark:text-gray-300">{t.aboutMe}</p>
             </div>
-            <div
-              className="mb-6 animate-fade-in-down"
-              ref={(el) => (sectionsRef.current[1] = el)}
-            >
-              <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+            <div className="mb-6" ref={(el) => (sectionsRef.current[1] = el)}>
+              <h2
+                className={`text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 ${
+                  language === "en" ? "border-blue-400" : "border-green-500"
+                } pb-2`}
+              >
                 {t.education}
               </h2>
               <p className="text-lg mb-4 dark:text-gray-300">{t.degree}</p>
               <p className="text-lg mb-4 dark:text-gray-300">{t.confucius}</p>
             </div>
-            <div
-              className="mb-6 animate-fade-in-down"
-              ref={(el) => (sectionsRef.current[2] = el)}
-            >
-              <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+            <div className="mb-6" ref={(el) => (sectionsRef.current[2] = el)}>
+              <h2
+                className={`text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 ${
+                  language === "en" ? "border-blue-400" : "border-green-500"
+                } pb-2`}
+              >
                 {t.experience}
               </h2>
               <p className="text-lg mb-4 dark:text-gray-300">
                 {t.experienceDetails}
               </p>
             </div>
-            <div
-              className="mb-6 animate-fade-in-down"
-              ref={(el) => (sectionsRef.current[3] = el)}
-            >
-              <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+            <div className="mb-6" ref={(el) => (sectionsRef.current[3] = el)}>
+              <h2
+                className={`text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 ${
+                  language === "en" ? "border-blue-400" : "border-green-500"
+                } pb-2`}
+              >
                 {t.skills}
               </h2>
               <ul className="text-lg mb-4 dark:text-gray-300 space-y-2">
                 {t.skillsList.map((skill, index) => (
-                  <li
-                    key={index}
-                    className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300"
-                  >
+                  <li key={index} className="flex items-center justify-between">
                     <span>{skill.name}</span> {renderStars(skill.rating)}
                   </li>
                 ))}
               </ul>
             </div>
-            <div
-              className="mb-6 animate-fade-in-down"
-              ref={(el) => (sectionsRef.current[4] = el)}
-            >
-              <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+            <div className="mb-6" ref={(el) => (sectionsRef.current[4] = el)}>
+              <h2
+                className={`text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 ${
+                  language === "en" ? "border-blue-400" : "border-green-500"
+                } pb-2`}
+              >
                 {t.languages}
               </h2>
               <ul className="text-lg mb-4 dark:text-gray-300 space-y-2">
                 {t.languageList.map((language, index) => (
-                  <li
-                    key={index}
-                    className="flex items-center justify-between transition-transform transform hover:scale-105 duration-300"
-                  >
-                    <span>{language.name}</span>{" "}
-                    {renderStars(language.rating)}
+                  <li key={index} className="flex items-center justify-between">
+                    <span>{language.name}</span> {renderStars(language.rating)}
                   </li>
                 ))}
               </ul>
             </div>
-            <div
-              className="mb-6 animate-fade-in-down"
-              ref={(el) => (sectionsRef.current[5] = el)}
-            >
-              <h2 className="text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 border-green-500 pb-2">
+            <div className="mb-6" ref={(el) => (sectionsRef.current[5] = el)}>
+              <h2
+                className={`text-2xl font-semibold text-primary dark:text-white mt-4 mb-2 border-b-2 ${
+                  language === "en" ? "border-blue-400" : "border-green-500"
+                } pb-2`}
+              >
                 {t.projects}
               </h2>
               <ul
@@ -256,15 +249,33 @@ const MainPage = () => {
                 {t.projectList.map((project, index) => (
                   <li
                     key={index}
-                    className="transition-transform transform hover:scale-105 duration-300"
+                    className="hover-scale transition-transform transform duration-300"
                   >
                     <a
                       target="_blank"
                       href={project.link}
-                      className="block text-black transition-colors duration-300 hover:text-green-700 dark:hover:text-green-500"
+                      className={`block text-black transition-colors duration-300 ${
+                        language === "en"
+                          ? "hover:text-blue-400"
+                          : "hover:text-green-500"
+                      } ${
+                        language === "en"
+                          ? "dark:hover:text-blue-400"
+                          : "dark:hover:text-green-500"
+                      }`}
                     >
                       <div>
-                        <span className="text-green-500 dark:text-green-300">
+                        <span
+                          className={`${
+                            language === "en"
+                              ? "text-blue-400"
+                              : "text-green-500"
+                          } ${
+                            language === "en"
+                              ? "dark:text-blue-400"
+                              : "dark:text-green-300"
+                          }`}
+                        >
                           {project.title}
                         </span>
                         <p className="text-sm dark:text-gray-400">
@@ -289,7 +300,9 @@ const MainPage = () => {
               href="https://www.linkedin.com/in/antonioniet/"
               target="_blank"
               rel="noopener noreferrer"
-              className="footer-icon"
+              className={`footer-icon no-hover ${
+                language === "en" ? "hover:text-blue-400" : ""
+              }`}
             >
               <FaLinkedin />
             </a>
@@ -297,13 +310,17 @@ const MainPage = () => {
               href="https://github.com/nietus"
               target="_blank"
               rel="noopener noreferrer"
-              className="footer-icon"
+              className={`footer-icon no-hover ${
+                language === "en" ? "hover:text-blue-400" : ""
+              }`}
             >
               <FaGithub />
             </a>
             <a
               href="mailto:antonio.couto@sga.pucminas.br"
-              className="footer-icon"
+              className={`footer-icon no-hover ${
+                language === "en" ? "hover:text-blue-400" : ""
+              }`}
             >
               <FaEnvelope />
             </a>
